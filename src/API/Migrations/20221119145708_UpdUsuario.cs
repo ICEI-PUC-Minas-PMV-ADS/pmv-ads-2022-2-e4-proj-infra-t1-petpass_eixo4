89450c828c29cc1e5e4bbd387788a02f21e11b4e
Rodrigo Lobenwein <62033829+rlobenwein@users.noreply.github.com>
2022-11-26 12:44:18 -0300

    Update API

---
+++ src/API/Migrations/20221119145708_UpdUsuario.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetPassBackend.Migrations
+{
+    public partial class UpdUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Usuarios",
+                newName: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Usuarios",
+                newName: "Nome");
+        }
+    }
+}
